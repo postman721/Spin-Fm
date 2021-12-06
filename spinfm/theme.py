0954c8ec921dd4b38463434844c3183a18bb4d81
JJ Posti <jjposti@techtimejourney.net>
2021-12-06 21:04:32 +0200

    Spin-

---
+++ spinfm/theme.py
@@ -0,0 +1,12 @@
+
+#Set your theme here by changing the theme file between " "
+
+#Supported values: 
+
+		#	"dark"	  # Default theme with blue colors.
+		# 	"blue"    # Blue theme.
+		#   ""		  # Basic QT without CSS theming.
+
+theme="dark"
+
+
